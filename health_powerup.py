922593e413ae773d707c53065de8e4f846e949d8
agustinmoses <magustin10@uchicago.edu>
2023-12-01 01:08:56 -0600

    Added the health powerup

---
+++ health_powerup.py
@@ -0,0 +1,20 @@
+import pygame
+from random import  randint
+
+
+class HealthPower(pygame.sprite.Sprite):
+
+    def __init__(self, screen_width):
+        super().__init__()
+        
+        self.image = pygame.image.load('graphics/shield_powerup.png').convert_alpha()
+        self.random_x_pos = randint(50,screen_width - 50)
+    
+        self.rect = self.image.get_rect(topleft = (self.random_x_pos,10))
+        self.speed = 2
+
+    def update(self):
+        
+        self.rect.y += self.speed
+        if self.rect.y > 600:
+            self.kill()
